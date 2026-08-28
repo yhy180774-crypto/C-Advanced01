b0e137ff8683568fcb394d6ba14943fb05d70009
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-29 02:12:02 +0300

    Add project files.

---
+++ C#Advanced01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Advanced01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
